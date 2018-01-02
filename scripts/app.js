2fa71b0c047ece9543bee3c92ee52ac8428321eb
Nazmi <nazmifeeroz@hotmail.com>
2018-01-02 08:43:19 +0800

    installed @uirouter. - note that angularjs is renamed to @uirouter - see changes to plugins first before install

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,19 @@
 angular
-  .module("ngClassifieds", ["ngMaterial"])
-  .config(function($mdThemingProvider) {
+  .module("ngClassifieds", ["ngMaterial", 'ui.router'])
+  .config(function($mdThemingProvider, $stateProvider) {
 
     $mdThemingProvider.theme('default')
       .primaryPalette('teal')
       .accentPalette('orange');
+
+    $stateProvider
+      .state('stateone', {
+        url: '/stateone',
+        template: '<h1>State one</h1>'
+      })
+      .state('statetwo', {
+        url: '/statetwo',
+        template: '<h1>state two</h1>'
+      });
 
   });
